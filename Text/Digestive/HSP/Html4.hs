db875e8aeea427e8897b3be65a7b350105fe9d4e
Jeremy Shaw <jeremy@seereason.com>
2011-05-01 15:41:00 +0000

    added inputCheckboxes. Change inputRadio to make the default value optional.

--- Text/Digestive/HSP/Html4.hs
+++ Text/Digestive/HSP/Html4.hs
@@ -3,6 +3,7 @@
 module Text.Digestive.HSP.Html4 where
 
 import Control.Applicative             ((<$>))
+import Control.Monad                   (mplus)
 import Data.Maybe                      (fromMaybe)
 import Data.Monoid                     (Monoid(mempty), mconcat)
 import Data.Text                       (Text)
@@ -11,7 +12,7 @@ import HSP                             (XMLGenerator, XMLGenT, EmbedAsChild(..),
 import qualified HSX.XMLGenerator      as HSX
 import Text.Digestive                   -- (Form, mapViews)
 import Text.Digestive.Common           as Common        -- (Form, mapViews)
-import Text.Digestive.Forms            as Forms -- (inputString, inputRead, inputBool, inputChoice)
+import Text.Digestive.Forms            as Forms hiding (inputChoice) -- (inputString, inputRead, inputBool, inputChoice)
 
 
 showFormId :: FormId -> String
@@ -61,23 +62,34 @@ inputPassword =
     flip Forms.inputString Nothing $ \id' inp ->
         [<input type="password" name=(showFormId id') id=(showFormId id') value=(fromMaybe "" inp) />]
 
+checked True = [("checked" := "checked")]
+checked False = []
+
 inputCheckBox :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
               => Bool
               -> Form m i e [XMLGenT x (HSX.XML x)] Bool
 inputCheckBox inp =
     flip inputBool inp $ \id' inp ->
-        [<input type="checkbox" name=(showFormId id') id=(showFormId id') checked />]
-    where
-      checked =
-          if inp
-          then [("checked" := "checked")]
-          else []
+        [<input type="checkbox" name=(showFormId id') id=(showFormId id') (checked inp) />]
+
+inputCheckboxes :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
+           => Bool                                 -- ^ Use @<br>@ tags
+           -> [a]                                  -- ^ Default option
+           -> [(a, c)]                             -- ^ Choices with their names
+           -> Form m i e [XMLGenT x (HSX.XML x)] [a] -- ^ Resulting form
+inputCheckboxes br defs choices =
+    inputChoices toView defs (map fst choices)
+  where
+    toView group' id' sel val =
+        [ <input type="checkbox" name=(showFormId group') id=id' value=id' />
+        , <label for=id'><% fromMaybe mempty $ lookup val choices %></label>
+        ] ++ if br then [<br />] else []
 
 inputRadio :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
            => Bool                                 -- ^ Use @<br>@ tags
-           -> a                                    -- ^ Default option
+           -> Maybe a                              -- ^ Default option
            -> [(a, c)]                             -- ^ Choices with their names
-           -> Form m i e [XMLGenT x (HSX.XML x)] a -- ^ Resulting form
+           -> Form m i e [XMLGenT x (HSX.XML x)] (Maybe a) -- ^ Resulting form
 inputRadio br def choices =
     inputChoice toView def (map fst choices)
   where
@@ -85,14 +97,13 @@ inputRadio br def choices =
         [ <input type="radio" name=(showFormId group') id=id' value=id' />
         , <label for=id'><% fromMaybe mempty $ lookup val choices %></label>
         ] ++ if br then [<br />] else []
+
 submit :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
           => String
           -> Form m i e [XMLGenT x (HSX.XML x)] String
 submit v = 
     Forms.inputString (\id' inp ->
         [<input type="submit" name=(showFormId id') id=(showFormId id') value=(fromMaybe "" inp) />]) (Just v)
-
-
 
 label :: (Monad m, XMLGenerator x, EmbedAsChild x c, EmbedAsAttr x (Attr String String))
       => c
@@ -139,3 +150,50 @@ setAttrs :: (EmbedAsAttr x attr, XMLGenerator x, Monad m, Functor m) =>
          -> attr 
          -> Form m i e [HSX.GenXML x] a
 setAttrs form attrs = mapView (map (`set` attrs)) form
+
+
+-- functions that really belong in digestive-functors
+
+
+inputChoice :: (Monad m, Functor m, FormInput i f, Monoid v, Eq a)
+            => (FormId -> String -> Bool -> a -> v)  -- ^ Choice constructor
+            -> Maybe a                               -- ^ Default option
+            -> [a]                                   -- ^ Choices
+            -> Form m i e v (Maybe a)                -- ^ Resulting form
+inputChoice toView defaultInput choices = Form $ do
+    inputKey <- fromMaybe "" . (getInputString =<<) <$> getFormInput
+    id' <- getFormId
+    formInput <- isFormInput
+    let -- Find the actual input, based on the key, or use the default input
+        inp = if formInput 
+              then lookup inputKey $ zip (ids id') choices
+              else defaultInput
+        -- Apply the toView' function to all choices
+        view' = mconcat $ zipWith (toView' id' inp) (ids id') choices
+    return (View (const view'), Ok inp)
+  where
+    ids id' = map (((show id' ++ "-") ++) . show) [1 .. length choices]
+    toView' id' inp key x = toView id' key (inp == Just x) x
+
+inputChoices :: (Monad m, Functor m, FormInput i f, Monoid v, Eq a)
+            => (FormId -> String -> Bool -> a -> v)  -- ^ Choice constructor
+            -> [a]                                   -- ^ Default options
+            -> [a]                                   -- ^ Choices
+            -> Form m i e v [a]                      -- ^ Resulting form
+inputChoices toView defaults choices = Form $ do
+    inputKey <- fromMaybe "" . (getInputString =<<) <$> getFormInput
+    id' <- getFormId
+    formInput <- isFormInput
+    let -- Find the actual input, based on the key, or use the default input
+        inps = if formInput 
+               then lookups inputKey $ zip (ids id') choices
+               else defaults
+        -- Apply the toView' function to all choices
+        view' = mconcat $ zipWith (toView' id' inps) (ids id') choices
+    return (View (const view'), Ok inps)
+  where
+    ids id' = map (((show id' ++ "-") ++) . show) [1 .. length choices]
+    toView' id' inps key x = toView id' key (x `elem` inps) x
+
+lookups :: (Eq a) => a -> [(a, b)] -> [b]
+lookups a = map snd . filter ((== a) . fst)
