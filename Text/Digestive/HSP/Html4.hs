e366bd6ccf267e561d5011e52ec7950d60383d4d
Jeremy Shaw <jeremy@seereason.com>
2011-05-03 21:05:24 +0000

    updates for select and  multi-select

--- Text/Digestive/HSP/Html4.hs
+++ Text/Digestive/HSP/Html4.hs
@@ -4,7 +4,7 @@ module Text.Digestive.HSP.Html4 where
 
 import Control.Applicative             ((<$>))
 import Control.Monad                   (mplus)
-import Data.Maybe                      (fromMaybe)
+import Data.Maybe                      (fromMaybe, mapMaybe)
 import Data.Monoid                     (Monoid(mempty), mconcat)
 import Data.Text                       (Text)
 import qualified Data.Text             as Text
@@ -65,6 +65,9 @@ inputPassword =
 checked True = [("checked" := "checked")]
 checked False = []
 
+selected True = [("selected" := "selected")]
+selected False = []
+
 inputCheckBox :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
               => Bool
               -> Form m i e [XMLGenT x (HSX.XML x)] Bool
@@ -81,7 +84,7 @@ inputCheckboxes br defs choices =
     inputChoices toView defs (map fst choices)
   where
     toView group' id' sel val =
-        [ <input type="checkbox" name=(showFormId group') id=id' value=id' />
+        [ <input type="checkbox" name=(showFormId group') id=id' value=id' (checked sel) />
         , <label for=id'><% fromMaybe mempty $ lookup val choices %></label>
         ] ++ if br then [<br />] else []
 
@@ -94,9 +97,35 @@ inputRadio br def choices =
     inputChoice toView def (map fst choices)
   where
     toView group' id' sel val =
-        [ <input type="radio" name=(showFormId group') id=id' value=id' />
+        [ <input type="radio" name=(showFormId group') id=id' value=id' (checked sel) />
         , <label for=id'><% fromMaybe mempty $ lookup val choices %></label>
         ] ++ if br then [<br />] else []
+
+inputSelect :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
+           => Maybe a                              -- ^ Default option
+           -> [(a, c)]                             -- ^ Choices with their names
+           -> Form m i e [XMLGenT x (HSX.XML x)] (Maybe a) -- ^ Resulting form
+inputSelect def choices = 
+    Form $ do id' <- getFormId
+              unForm $ mapView (\cs -> [<select name=(showFormId id') id=(showFormId id')><% cs %></select>])
+                         (inputChoice toView def (map fst choices))
+  where
+    toView group' id' sel val =
+        [ <option id=id' value=id' (selected sel)><% fromMaybe mempty $ lookup val choices %></option>]
+
+inputMultiSelect :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
+           => [a]                                  -- ^ Default options
+           -> [(a, c)]                             -- ^ Choices with their names
+           -> Form m i e [XMLGenT x (HSX.XML x)] [a] -- ^ Resulting form
+inputMultiSelect defs choices = 
+    Form $ do id' <- getFormId
+              unForm $ mapView (\cs -> [<select multiple="" name=(showFormId id') id=(showFormId id')><% cs %></select>])
+                         (inputChoices toView defs (map fst choices))
+  where
+    toView group' id' sel val =
+        [ <option id=id' value=id' (selected sel)><% fromMaybe mempty $ lookup val choices %></option>]
+
+
 
 submit :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
           => String
@@ -181,12 +210,12 @@ inputChoices :: (Monad m, Functor m, FormInput i f, Monoid v, Eq a)
             -> [a]                                   -- ^ Choices
             -> Form m i e v [a]                      -- ^ Resulting form
 inputChoices toView defaults choices = Form $ do
-    inputKey <- fromMaybe "" . (getInputString =<<) <$> getFormInput
+    inputKeys <- maybe [] getInputStrings <$> getFormInput
     id' <- getFormId
     formInput <- isFormInput
     let -- Find the actual input, based on the key, or use the default input
         inps = if formInput 
-               then lookups inputKey $ zip (ids id') choices
+               then mapMaybe (\inputKey -> lookup inputKey $ zip (ids id') choices) inputKeys
                else defaults
         -- Apply the toView' function to all choices
         view' = mconcat $ zipWith (toView' id' inps) (ids id') choices
