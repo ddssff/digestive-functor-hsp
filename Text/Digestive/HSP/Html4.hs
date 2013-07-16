e0667403d58e764f034e14ff3506b1a009736435
Jeremy Shaw <jeremy@seereason.com>
2013-07-16 17:42:41 +0000

    0.6.0: updated to hsp-0.9

--- Text/Digestive/HSP/Html4.hs
+++ Text/Digestive/HSP/Html4.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# OPTIONS_GHC -F -pgmFtrhsx #-}
+{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
+{-# OPTIONS_GHC -F -pgmFhsx2hs #-}
 module Text.Digestive.HSP.Html4 where
 
 import Control.Applicative             ((<$>))
@@ -8,74 +8,76 @@ import Data.Maybe                      (fromMaybe, mapMaybe)
 import Data.Monoid                     (Monoid(mempty), mconcat)
 import Data.Text                       (Text)
 import qualified Data.Text             as Text
-import HSP                             (XMLGenerator, XMLGen(..), XMLGenT, EmbedAsChild(..), EmbedAsAttr(..), Attr(..), set)
-import HSX.XMLGenerator                (GenXML)
+import qualified Data.Text.Lazy        as TL
+import HSP                             (XMLGenerator, XMLGen(..), GenXML, XMLGenT, EmbedAsChild(..), EmbedAsAttr(..), Attr(..), fromStringLit, set)
 import Text.Digestive                  (FormId, Form(..), Result(..), View(..), getFormId, getFormInput, isFormInput, mapView)
 import Text.Digestive.Common           as Common
 import Text.Digestive.Forms            as Forms
 
-showFormId :: FormId -> String
-showFormId id' = show id'
+showFormId :: FormId -> TL.Text
+showFormId id' = TL.pack $ show id'
 
 -- text input box that returns a 'String'
-inputString :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
+inputString :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text)
           => Maybe String -- ^ initial value
           -> Form m i e [XMLGenT x (XMLType x)] String
-inputString = 
+inputString =
     Forms.inputString $ \id' inp ->
-        [<input type="text" name=(showFormId id') id=(showFormId id') value=(fromMaybe "" inp) />]
+        [<input type="text" name=(showFormId id') id=(showFormId id') value=(maybe TL.empty TL.pack inp) />]
 
 -- FIXME: use inputText from Forms when it becomes available
-inputText :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
+inputText :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text)
           => Maybe Text
           -> Form m i e [XMLGenT x (XMLType x)] Text
-inputText v = 
+inputText v =
     Text.pack <$>
         ((Forms.inputString $ \id' inp ->
-            [<input type="text" name=(showFormId id') id=(showFormId id') value=(fromMaybe "" inp) />]) (Text.unpack <$> v))
+            [<input type="text" name=(showFormId id') id=(showFormId id') value=(maybe TL.empty TL.pack inp) />]) (Text.unpack <$> v))
 
-inputTextArea :: (Monad m, Functor m, XMLGenerator x, FormInput i f) =>
+inputTextArea :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text) =>
                  Maybe Int -- ^ cols
               -> Maybe Int -- ^ rows
               -> Maybe String
               -> Form m i e [XMLGenT x (XMLType x)] String
-inputTextArea c r = 
+inputTextArea c r =
     Forms.inputString $ \id' inp ->
-        [<textarea name=(showFormId id') id=(showFormId id') (rows r ++ cols c)><% fromMaybe "" inp %></textarea>]
+        [<textarea name=(showFormId id') id=(showFormId id') (rows r ++ cols c)><% maybe TL.empty TL.pack inp %></textarea>]
     where
       rows Nothing  = []
-      rows (Just n) = [("rows" := n)]
+      rows (Just n) = [(TL.pack "rows" := n)]
       cols Nothing  = []
-      cols (Just n) = [("cols" := n)]
+      cols (Just n) = [(TL.pack "cols" := n)]
 
-inputTextRead :: (Monad m, Functor m, Show a, Read a, XMLGenerator x, FormInput i f)
+inputTextRead :: (Monad m, Functor m, Show a, Read a, XMLGenerator x, FormInput i f, StringType x ~ TL.Text)
               => String
               -> Maybe a
               -> Form m i String [XMLGenT x (XMLType x)] a
 inputTextRead error' =
     flip inputRead error' $ \id' inp ->
-        [<input type="text" name=(showFormId id') id=(showFormId id') value=(fromMaybe "" inp) />]
+        [<input type="text" name=(showFormId id') id=(showFormId id') value=(maybe TL.empty TL.pack inp) />]
 
-inputPassword :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
+inputPassword :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text)
               => Form m i e [XMLGenT x (XMLType x)] String
 inputPassword =
     flip Forms.inputString Nothing $ \id' inp ->
-        [<input type="password" name=(showFormId id') id=(showFormId id') value=(fromMaybe "" inp) />]
+        [<input type="password" name=(showFormId id') id=(showFormId id') value=(maybe TL.empty TL.pack inp) />]
 
-checked True = [("checked" := "checked")]
+checked :: Bool ->  [Attr TL.Text TL.Text]
+checked True = [(TL.pack "checked" := TL.pack "checked")]
 checked False = []
 
-selected True = [("selected" := "selected")]
+selected :: Bool ->  [Attr TL.Text TL.Text]
+selected True = [(TL.pack "selected" := TL.pack "selected")]
 selected False = []
 
-inputCheckBox :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
+inputCheckBox :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text)
               => Bool
               -> Form m i e [XMLGenT x (XMLType x)] Bool
 inputCheckBox inp =
     flip inputBool inp $ \id' inp ->
         [<input type="checkbox" name=(showFormId id') id=(showFormId id') (checked inp) />]
 
-inputCheckboxes :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
+inputCheckboxes :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f, StringType x ~ TL.Text)
            => Bool                                 -- ^ Use @<br>@ tags
            -> [a]                                  -- ^ Default option
            -> [(a, c)]                             -- ^ Choices with their names
@@ -84,8 +86,8 @@ inputCheckboxes br defs choices =
     inputChoices toView defs (map fst choices)
   where
     toView group' id' sel val =
-        [ <input type="checkbox" name=(showFormId group') id=id' value=id' (checked sel) />
-        , <label for=id'><% fromMaybe mempty $ lookup val choices %></label>
+        [ <input type="checkbox" name=(showFormId group') id=(TL.pack id') value=(TL.pack id') (checked sel) />
+        , <label for=(TL.pack id')><% fromMaybe mempty $ lookup val choices %></label>
         ] ++ if br then [<br />] else []
 
 -- | radio buttons
@@ -97,7 +99,7 @@ inputCheckboxes br defs choices =
 -- buttons should be able to have an undefined state. But since
 -- user-agents are inconsistent about handling undefined, it is
 -- recommended that a default option always be provided.
-inputRadio :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
+inputRadio :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f, StringType x ~ TL.Text)
            => Bool                                 -- ^ Use @<br>@ tags
            -> a                                    -- ^ Default option
            -> [(a, c)]                             -- ^ Choices with their names
@@ -106,65 +108,65 @@ inputRadio br def choices =
     inputChoice toView def (map fst choices)
   where
     toView group' id' sel val =
-        [ <input type="radio" name=(showFormId group') id=id' value=id' (checked sel) />
-        , <label for=id'><% fromMaybe mempty $ lookup val choices %></label>
+        [ <input type="radio" name=(showFormId group') id=(TL.pack id') value=(TL.pack id') (checked sel) />
+        , <label for=(TL.pack id')><% fromMaybe mempty $ lookup val choices %></label>
         ] ++ if br then [<br />] else []
 
-inputSelect :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
+inputSelect :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f, StringType x ~ TL.Text)
            => a                                    -- ^ Default option
            -> [(a, c)]                             -- ^ Choices with their names
            -> Form m i e [XMLGenT x (XMLType x)] a -- ^ Resulting form
-inputSelect def choices = 
+inputSelect def choices =
     Form $ do id' <- getFormId
               unForm $ mapView (\cs -> [<select name=(showFormId id') id=(showFormId id')><% cs %></select>])
                          (inputChoice toView def (map fst choices))
   where
     toView group' id' sel val =
-        [ <option id=id' value=id' (selected sel)><% fromMaybe mempty $ lookup val choices %></option>]
+        [ <option id=(TL.pack id') value=(TL.pack id') (selected sel)><% fromMaybe mempty $ lookup val choices %></option>]
 
-inputMultiSelect :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f)
+inputMultiSelect :: (Monad m, Functor m, Eq a, XMLGenerator x, EmbedAsChild x c, Monoid c, FormInput i f, StringType x ~ TL.Text)
            => [a]                                  -- ^ Default options
            -> [(a, c)]                             -- ^ Choices with their names
            -> Form m i e [XMLGenT x (XMLType x)] [a] -- ^ Resulting form
-inputMultiSelect defs choices = 
+inputMultiSelect defs choices =
     Form $ do id' <- getFormId
               unForm $ mapView (\cs -> [<select multiple="" name=(showFormId id') id=(showFormId id')><% cs %></select>])
                          (inputChoices toView defs (map fst choices))
   where
     toView group' id' sel val =
-        [ <option id=id' value=id' (selected sel)><% fromMaybe mempty $ lookup val choices %></option>]
+        [ <option id=(TL.pack id') value=(TL.pack id') (selected sel)><% fromMaybe mempty $ lookup val choices %></option>]
 
 
 
-submit :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
+submit :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text)
           => String
           -> Form m i e [XMLGenT x (XMLType x)] String
-submit v = 
+submit v =
     Forms.inputString (\id' inp ->
-        [<input type="submit" name=(showFormId id') id=(showFormId id') value=(fromMaybe "" inp) />]) (Just v)
+        [<input type="submit" name=(showFormId id') id=(showFormId id') value=(maybe TL.empty TL.pack inp) />]) (Just v)
 
 -- TODO: add hiddenText when new digestive functors is availabe on hackage that adds Forms.inputText
-inputHiddenString :: (Monad m, Functor m, XMLGenerator x, FormInput i f)
+inputHiddenString :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text)
           => String
           -> Form m i e [XMLGenT x (XMLType x)] String
-inputHiddenString str = 
-    Forms.inputString 
+inputHiddenString str =
+    Forms.inputString
              (\id' inp ->
-                  [<input type="hidden" name=(show id') id=(show id') value=(fromMaybe "" inp) />])
+                  [<input type="hidden" name=(TL.pack $ show id') id=(TL.pack $ show id') value=(maybe TL.empty TL.pack inp) />])
              (Just str)
 -- | file upload form
-inputFile :: (Monad m, Functor m, XMLGenerator x, FormInput i f) => 
+inputFile :: (Monad m, Functor m, XMLGenerator x, FormInput i f, StringType x ~ TL.Text) =>
              Form m i e [XMLGenT x (XMLType x)] (Maybe f)
-inputFile = Forms.inputFile $ \id' -> [<input type="file" name=(show id') id=(show id') />]
+inputFile = Forms.inputFile $ \id' -> [<input type="file" name=(TL.pack $ show id') id=(TL.pack $ show id') />]
 
-label :: (Monad m, XMLGenerator x, EmbedAsChild x c, EmbedAsAttr x (Attr String String))
+label :: (Monad m, XMLGenerator x, EmbedAsChild x c, EmbedAsAttr x (Attr TL.Text TL.Text), StringType x ~ TL.Text)
       => c
       -> Form m i e [XMLGenT x (XMLType x)] ()
 label string =
     Common.label $ \id' ->
         [<label for=(showFormId id')><% string %></label>]
 
-errorList :: (XMLGenerator x, EmbedAsChild x c, EmbedAsChild x [XMLType x]) => [c] -> [XMLGenT x (XMLType x)]
+errorList :: (XMLGenerator x, EmbedAsChild x c, EmbedAsChild x [XMLType x], StringType x ~ TL.Text) => [c] -> [XMLGenT x (XMLType x)]
 errorList [] = []
 errorList children =
     [<ul>
@@ -172,11 +174,11 @@ errorList children =
      </ul>
     ]
 
-errors :: (Monad m, XMLGenerator x, EmbedAsChild x [XMLType x]) => 
+errors :: (Monad m, XMLGenerator x, EmbedAsChild x [XMLType x], StringType x ~ TL.Text) =>
           Form m i String [XMLGenT x (XMLType x)] ()
 errors = Common.errors errorList
 
-childErrors :: (Monad m, XMLGenerator x, EmbedAsChild x [XMLType x]) => 
+childErrors :: (Monad m, XMLGenerator x, EmbedAsChild x [XMLType x], StringType x ~ TL.Text) =>
                Form m i String [XMLGenT x (XMLType x)] ()
 childErrors = Common.childErrors errorList
 
@@ -189,17 +191,17 @@ testForm :: (Show a) => Form IO String String [XMLGenT Identity XML] a -> IO ()
 testForm form =
     do (view', result) <- runForm form (Environment $ const (return Nothing))
        case result of
-         (Ok a) -> 
+         (Ok a) ->
              do putStrLn $ "the result: " ++ show a
                 mapM_ (putStrLn . renderAsHTML . runIdentity . unXMLGenT ) $ unView view' []
-         (Error e) -> 
+         (Error e) ->
              do print e
                 mapM_ (putStrLn . renderAsHTML . runIdentity . unXMLGenT ) $ unView view' e
 -}
 
 setAttrs :: (EmbedAsAttr x attr, XMLGenerator x, Monad m, Functor m) =>
-            Form m i e [GenXML x] a 
-         -> attr 
+            Form m i e [GenXML x] a
+         -> attr
          -> Form m i e [GenXML x] a
 setAttrs form attrs = mapView (map (`set` attrs)) form
 
@@ -216,11 +218,11 @@ lookups a = map snd . filter ((== a) . fst)
 --
 -- @accept-charset="UTF-8"
 --
-form :: (XMLGenerator x, EmbedAsAttr x (Attr String action), EmbedAsChild x c) => 
+form :: (XMLGenerator x, EmbedAsAttr x (Attr TL.Text action), EmbedAsChild x c, StringType x ~ TL.Text) =>
         action -- ^ value for action attribute
      -> c      -- ^ contents of form tag
      -> XMLGenT x (XMLType x)
-form action xml = 
+form action xml =
     <form action=action method="POST" enctype="multipart/form-data" accept-charset="UTF-8">
       <% xml %>
     </form>
